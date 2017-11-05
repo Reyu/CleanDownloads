902cac29860ecdac309fe8c0b18243cad9d3aee7
Tim Millican <tim.millican@blackfoxcode.com>
2017-11-05 12:32:51 -0500

    Initial Commit

---
+++ app/Main.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE MultiWayIf #-}
+
+module Main
+  (main)
+  where
+
+import Control.Monad (when, unless)
+import Data.Time.Clock (getCurrentTime, diffUTCTime)
+import System.Directory
+import System.FilePath ((</>))
+
+main :: IO ()
+main = do
+  getHomeDirectory >>= setCurrentDirectory . flip (</>) "Downloads"
+  createDirectoryIfMissing True "Old"
+  getDirectoryContents "." >>= clean' False ["Old", "Keep"]
+  setCurrentDirectory "Old"
+  getDirectoryContents "." >>= clean' True []
+  where
+    clean' remove extra = mapM_ (clean remove) . filter (noDotsPlus extra)
+    noDotsPlus extra = not . flip elem ([".", ".."] ++ extra)
+
+clean :: Bool -> FilePath -> IO ()
+clean remove path = do
+  cTime <- getCurrentTime
+  aTime <- getAccessTime path
+  isFile <- doesFileExist path -- True: File | False: Directory
+  let newPath = "Old" </> path
+  when (diffUTCTime cTime aTime > (60 * 60 * 24 * 7)) $
+    do if |  isFile && remove -> removeFile path
+          |  isFile && not remove -> renameFile path newPath
+          |  not isFile && remove -> removeDirectoryRecursive path
+          |  not isFile && not remove -> renameDirectory path newPath
+       unless remove $ setAccessTime newPath cTime
